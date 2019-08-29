3443314002b121717bc020fd82b0bda72afc6f21
ngrassmeyer <54650033+ngrassmeyer@users.noreply.github.com>
2019-08-28 19:57:06 -0500

    Add files via upload

---
+++ labs/lab_one.cpp
@@ -0,0 +1,15 @@
+/*Name: Noah Grassmeyer
+WSUID: P595Z792
+HW #: Lab 1
+Description: Print Hello World*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World";
+	return 0;
+}
+
